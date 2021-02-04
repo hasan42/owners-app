fd3dc87ffb00427275046abef30ba43e87c40445
rpokosov <hasan83842@gmail.com>
2021-02-04 21:18:05 +0300

    add pagination

--- src/components/base-ui/owners/Owners.jsx
+++ src/components/base-ui/owners/Owners.jsx
@@ -5,203 +5,30 @@ import { format, parseISO } from "date-fns";
 
 import personImage from "../../../assets/images/person.png";
 
-export const Owners = () => {
-  const memoryArr = [
-    {
-      name: "Savannah Nguyen",
-      date: new Date(2022, 1, 11),
-      profit: "328.85",
-      losser: "779.58",
-      phone: "(603) 555-0123",
-    },
-    {
-      name: "Jenny Wilson",
-      date: new Date(2022, 1, 11),
-      profit: "948.55",
-      losser: "589.99",
-      phone: "(629) 555-0129",
-    },
-    {
-      name: "Annette Black",
-      date: new Date(2022, 1, 11),
-      profit: "778.35",
-      losser: "948.55",
-      phone: "(308) 555-0121",
-    },
-    {
-      name: "Kathryn Murphy",
-      date: new Date(2022, 1, 11),
-      profit: "169.43",
-      losser: "202.87",
-      phone: "(302) 555-0107",
-    },
-    {
-      name: "Cameron Williamson",
-      date: new Date(2022, 1, 11),
-      profit: "782.01",
-      losser: "710.68",
-      phone: "(303) 555-0105",
-    },
-    {
-      name: "Kristin Watson",
-      date: new Date(2022, 1, 11),
-      profit: "767.50",
-      losser: "779.58",
-      phone: "(225) 555-0118",
-    },
-    {
-      name: "Eleanor Pena",
-      date: new Date(2022, 1, 11),
-      profit: "475.22",
-      losser: "219.78",
-      phone: "(205) 555-0100",
-    },
-    {
-      name: "Guy Hawkins",
-      date: new Date(2022, 1, 11),
-      profit: "576.28",
-      losser: "446.61",
-      phone: "(207) 555-0119",
-    },
-    {
-      name: "Brooklyn Simmons",
-      date: new Date(2022, 1, 11),
-      profit: "219.78",
-      losser: "928.41",
-      phone: "(201) 555-0124",
-    },
-    {
-      name: "Robert Fox",
-      date: new Date(2022, 1, 11),
-      profit: "202.87",
-      losser: "275.43",
-      phone: "(684) 555-0102",
-    },
-  ];
-
-  // const listRender = React.useMemo(() => {
-  //   const memoryArr = [
-  //     {
-  //       name: "Savannah Nguyen",
-  //       date: new Date(2022, 1, 11),
-  //       profit: "328.85",
-  //       losser: "779.58",
-  //       phone: "(603) 555-0123",
-  //     },
-  //     {
-  //       name: "Jenny Wilson",
-  //       date: new Date(2022, 1, 11),
-  //       profit: "948.55",
-  //       losser: "589.99",
-  //       phone: "(629) 555-0129",
-  //     },
-  //     {
-  //       name: "Annette Black",
-  //       date: new Date(2022, 1, 11),
-  //       profit: "778.35",
-  //       losser: "948.55",
-  //       phone: "(308) 555-0121",
-  //     },
-  //     {
-  //       name: "Kathryn Murphy",
-  //       date: new Date(2022, 1, 11),
-  //       profit: "169.43",
-  //       losser: "202.87",
-  //       phone: "(302) 555-0107",
-  //     },
-  //     {
-  //       name: "Cameron Williamson",
-  //       date: new Date(2022, 1, 11),
-  //       profit: "782.01",
-  //       losser: "710.68",
-  //       phone: "(303) 555-0105",
-  //     },
-  //     {
-  //       name: "Kristin Watson",
-  //       date: new Date(2022, 1, 11),
-  //       profit: "767.50",
-  //       losser: "779.58",
-  //       phone: "(225) 555-0118",
-  //     },
-  //     {
-  //       name: "Eleanor Pena",
-  //       date: new Date(2022, 1, 11),
-  //       profit: "475.22",
-  //       losser: "219.78",
-  //       phone: "(205) 555-0100",
-  //     },
-  //     {
-  //       name: "Guy Hawkins",
-  //       date: new Date(2022, 1, 11),
-  //       profit: "576.28",
-  //       losser: "446.61",
-  //       phone: "(207) 555-0119",
-  //     },
-  //     {
-  //       name: "Brooklyn Simmons",
-  //       date: new Date(2022, 1, 11),
-  //       profit: "219.78",
-  //       losser: "928.41",
-  //       phone: "(201) 555-0124",
-  //     },
-  //     {
-  //       name: "Robert Fox",
-  //       date: new Date(2022, 1, 11),
-  //       profit: "202.87",
-  //       losser: "275.43",
-  //       phone: "(684) 555-0102",
-  //     },
-  //   ];
-  //   const localArr = localStorage.getItem("rpokosov-metamins")
-  //     ? JSON.parse(localStorage.getItem("rpokosov-metamins"))
-  //     : [];
-  //   const list = memoryArr.concat(localArr);
-
-  //   return list.map((item, i) => {
-  //     return (
-  //       <div className="owner" key={i}>
-  //         <div>
-  //           <img src={personImage} alt={item.name} /> {item.name}
-  //         </div>
-  //         <div>
-  //           {format(
-  //             typeof item.date === "string" ? parseISO(item.date) : item.date,
-  //             "MM/dd/yy"
-  //           )}
-  //         </div>
-  //         <div>{Numeral(item.profit).format("$0,0.00")}</div>
-  //         <div>{Numeral(item.losser).format("$0,0.00")}</div>
-  //         <div>{item.phone}</div>
-  //       </div>
-  //     );
-  //   });
-  // }, []);
-
-  const renderList = React.useCallback(() => {
-    const localArr = localStorage.getItem("rpokosov-metamins")
-      ? JSON.parse(localStorage.getItem("rpokosov-metamins"))
-      : [];
-    const list = memoryArr.concat(localArr);
-
-    return list.map((item, i) => {
-      return (
-        <div className="owner" key={i}>
-          <div>
-            <img src={personImage} alt={item.name} /> {item.name}
+export const Owners = ({ items, page }) => {
+  const renderList = React.useMemo(() => {
+    return items.map((item, i) => {
+      if (i >= (page - 1) * 10 && i < (page - 1) * 10 + 10) {
+        return (
+          <div className="owner" key={i}>
+            <div>
+              <img src={personImage} alt={item.name} /> {item.name}
+            </div>
+            <div>
+              {format(
+                typeof item.date === "string" ? parseISO(item.date) : item.date,
+                "MM/dd/yy"
+              )}
+            </div>
+            <div>{Numeral(item.profit).format("$0,0.00")}</div>
+            <div>{Numeral(item.losser).format("$0,0.00")}</div>
+            <div>{item.phone}</div>
           </div>
-          <div>
-            {format(
-              typeof item.date === "string" ? parseISO(item.date) : item.date,
-              "MM/dd/yy"
-            )}
-          </div>
-          <div>{Numeral(item.profit).format("$0,0.00")}</div>
-          <div>{Numeral(item.losser).format("$0,0.00")}</div>
-          <div>{item.phone}</div>
-        </div>
-      );
+        );
+      }
+      return null;
     });
-  });
+  }, [page, items]);
 
   return (
     <div className="owners">
@@ -212,7 +39,7 @@ export const Owners = () => {
         <div>Losses</div>
         <div>Phone</div>
       </div>
-      {renderList()}
+      {renderList}
     </div>
   );
 };
--- src/components/pages/home-page/HomePage.jsx
+++ src/components/pages/home-page/HomePage.jsx
@@ -1,18 +1,33 @@
 import * as React from "react";
 import "./home-page.scss";
 import Modal from "react-modal";
+import Pagination from "rc-pagination";
 
 import { BaseLayout } from "../../layouts/base-layout";
 import { ReactComponent as DotsIcon } from "../../../assets/icons/dots.svg";
 import { ReactComponent as CloseIcon } from "../../../assets/icons/close.svg";
+import { ReactComponent as ArrowIcon } from "../../../assets/icons/arrow.svg";
 
 import { Owners } from "../../base-ui/owners";
 import { AddOwner } from "../../base-ui/add-owner";
+import { memoryArr } from "./owner";
 
 Modal.setAppElement("#root");
 
 export const HomePage = () => {
-  const ownerList = React.useRef(null);
+  const [ownerList, setOwnerList] = React.useState([]);
+  const [currentPage, setCurrentPage] = React.useState(1);
+
+  const divItemRender = (current, type, element) => {
+    if (type === "prev") {
+      return <ArrowIcon />;
+    }
+    if (type === "next") {
+      return <ArrowIcon />;
+    }
+    return element;
+  };
+
   const [modalIsOpen, setIsOpen] = React.useState(false);
   const closeModal = React.useCallback(() => {
     setIsOpen(false);
@@ -20,6 +35,28 @@ export const HomePage = () => {
   const openModal = React.useCallback(() => {
     setIsOpen(true);
   }, []);
+
+  const renderList = React.useCallback(() => {
+    const localArr = localStorage.getItem("rpokosov-metamins")
+      ? JSON.parse(localStorage.getItem("rpokosov-metamins"))
+      : [];
+    const list = memoryArr.concat(localArr);
+
+    setOwnerList(list);
+  }, []);
+
+  const handlerSubmit = React.useCallback(() => {
+    closeModal();
+    renderList();
+  }, [closeModal, renderList]);
+
+  const handlePageClick = React.useCallback((page) => {
+    setCurrentPage(page);
+  }, []);
+
+  React.useEffect(() => {
+    renderList();
+  }, [renderList]);
 
   const customStyles = {
     content: {
@@ -49,7 +86,27 @@ export const HomePage = () => {
         </div>
       </header>
       <section>
-        <Owners ref={ownerList} />
+        <Owners items={ownerList} page={currentPage} />
+
+        {ownerList.length > 10 && (
+          <Pagination
+            itemRender={divItemRender}
+            onChange={handlePageClick}
+            current={currentPage}
+            total={ownerList.length}
+            showTotal={(total, range) => (
+              <>
+                <div className="rc-pagination-total-text-item">
+                  {currentPage}
+                </div>{" "}
+                of{" "}
+                <div className="rc-pagination-total-text-item">
+                  {Math.ceil(ownerList.length / 10)}
+                </div>
+              </>
+            )}
+          />
+        )}
       </section>
       <Modal
         isOpen={modalIsOpen}
@@ -59,12 +116,7 @@ export const HomePage = () => {
         <button className="close-modal" onClick={closeModal}>
           <CloseIcon />
         </button>
-        <AddOwner
-          onSubmitForm={() => {
-            closeModal();
-            ownerList.current.renderList();
-          }}
-        />
+        <AddOwner onSubmitForm={handlerSubmit} />
       </Modal>
     </BaseLayout>
   );
